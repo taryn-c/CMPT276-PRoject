5f2c8ef5a8bd991c957f33a2ab72be238f5b8b82
Chris Przeworski <chris.przeworski@gmail.com>
2019-06-26 23:17:51 -0700

    Revert "Got rid of all heroku demo source code"
    This reverts commit 7db47265251ad903f44bf3a6705b2bf49d875f8c.

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-const express=require('express');
-const path= require('path');
-const PORT= 5000;
-const app= express();
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-//connect to postgres
-
-
-
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
