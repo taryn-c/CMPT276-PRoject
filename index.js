5dcf1d0f18b8c222c41d0dd64ccbcae47b36e960
cjprzewo <cjprzewo@cs-sc4080u-d02.cs.surrey.sfu.ca>
2019-06-28 17:40:41 -0700

    fixed database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const expressSession = require('express-session')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 const Pool = require('pg').Pool;
 
 const ADMIN_LEVEL_NOT_ADMIN = 0;
